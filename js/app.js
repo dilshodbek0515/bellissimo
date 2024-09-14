1ad9cf533fe7fdcd786d1b77fe75f6fc5950b06c
dilshod <dilshodx516@gmail.com>
2024-09-15 03:51:12 +0500

    bellissimo

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+// login
+const kirishEL = document.querySelector(".kirish")
+
+function kirishEl() {
+    kirishEL.style.display = "block";
+}
+function chiqishEl() {
+    kirishEL.style.display = "none";
+}
+
+// swiper
 var swiper = new Swiper(".mySwiper", {
     pagination: {
         el: ".swiper-pagination",
