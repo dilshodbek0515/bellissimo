5a2fe38e6f69c0249971d0b3a899f88b87705a97
dilshod <dilshodx516@gmail.com>
2024-09-15 20:09:45 +0500

    bellissimo

--- js/app.js
+++ js/app.js
@@ -32,3 +32,100 @@ var swiper = new Swiper(".mySwiper", {
 //     console.log(isAllCaps("ACSKLDFJSGSKLDFJSKLDFJ"));
 //     console.log(isAllCaps(""));
 // }
+
+
+// N2
+// function reverseWords(str) {
+
+//     let words = str.split(' ');
+//     let result = [];
+  
+//     for (let i = 0; i < words.length; i++) {
+//       let reversedWord = '';
+
+//       for (let j = words[i].length - 1; j >= 0; j--) {
+//         reversedWord += words[i][j];
+//       }
+  
+//         result.push(reversedWord);
+//     }
+//     return result.join(' ');
+//   }
+  
+//   console.log(reverseWords("This is an example!")); 
+  
+  
+// N3
+
+// const reverseSeq = n => {
+//     const result = [];
+    
+//     for (let i = n; i >= 1; i--) {
+//       result.push(i);
+//     }
+  
+//     return result;
+//   };
+  
+//   console.log(reverseSeq(5));
+
+
+// N4
+
+// function reverseList(list) {
+//     const reversed = [];
+    
+//     for (let i = list.length - 1; i >= 0; i--) {
+//       reversed.push(list[i]);
+//     }
+  
+//     return reversed;
+//   }
+  
+//   console.log(reverseList([1, 2, 3, 4]));
+
+
+
+// N5
+
+// function reverse(string) {
+    
+//     return string.split(' ').filter(word => word !== '').reverse().join(' ');          
+//   }
+  
+//   console.log(reverse("Hello World"));
+
+
+
+// N6
+
+// function digitize(n) {
+//     return String(n).split('').map(Number).reverse();
+//   }
+
+//   console.log(digitize(35231));
+
+
+
+// N7
+
+// function reverseWords(str) {
+
+//     const words = str.split(' ');
+//     const reversedWords = [];
+  
+
+//     for (let i = words.length - 1; i >= 0; i--) {
+//       reversedWords.push(words[i]);
+//     }
+  
+//     return reversedWords.join(' ');
+//   }
+  
+//   console.log(reverseWords("The greatest victory is that which requires no battle")); 
+
+// N8
+
+// const items = [];
+// items.push({a: "b", c: "d"});
+// console.log(items);  
